b47297e1a82edb21c7f217bc8a74105ffaf241b1
Luiz Passos <ahqnseioq@gmail.com>
2026-02-19 19:14:22 -0300

    chore(dialog_system): dynamic portrait change

--- Scripts/dialogo.gd
+++ Scripts/dialogo.gd
@@ -5,8 +5,11 @@ signal click(event)
 @export var nome: Label
 @export var texto_caixa = Label
 @export var caixa_dialogo = Panel
+@export var imagem_portrait = TextureRect
 
-func extrai_dialogos(file_path):
+var portrait_json = "res://Sistema/dialogo_portraits.json"
+
+func extrai_json(file_path):
 	var json = FileAccess.open(file_path, FileAccess.READ)
 	var conteudo = json.get_as_text()
 	var resultado = JSON.parse_string(conteudo)
@@ -15,16 +18,17 @@ func extrai_dialogos(file_path):
 func muda_texto(campo, texto):
 	campo.text = texto
 
-
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		click.emit(event)
 
 
 func muda_display(botao, json, personagem):
-	var dialogos = extrai_dialogos(json)
+	var dialogos = extrai_json(json)
 	var dialogo_personagem = dialogos[personagem]
+	var portrait = extrai_json(portrait_json)
 	
+	imagem_portrait.texture = load(portrait[personagem])
 	botao.visible = false
 	caixa_dialogo.visible = true
 	
@@ -35,3 +39,4 @@ func muda_display(botao, json, personagem):
 		await click
 	botao.visible = true
 	caixa_dialogo.visible = false
+	

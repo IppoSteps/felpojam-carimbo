9cd531beda94a9c6cfa7c7f5696381ab2058dc88
Luiz Passos <ahqnseioq@gmail.com>
2026-02-22 15:51:44 -0300

    chore/only-1-stamp

--- Scripts/sis_carimbos.gd
+++ Scripts/sis_carimbos.gd
@@ -6,7 +6,11 @@ signal click(event)
 @export var carimbo_1: TextureRect
 @export var carimbo_2: TextureRect
 
-var carimbos_json = "res://Sistema/carimbos_personagens.json"
+var carimbos_path := "res://Sistema/carimbos_personagens.json"
+var carimbos_json := {}
+
+func _ready():
+	carimbos_json = extrai_json(carimbos_path)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -19,17 +23,21 @@ func extrai_json(file_path):
 	return resultado
 
 func define_carimbo(personagem):
-	var json = extrai_json(carimbos_json)
-	var carimbo = json[personagem]
+	var carimbo = carimbos_json[personagem]
 	var carimbo_path = carimbo["path"]
 	var slots = get_tree().get_nodes_in_group("carimbo_slots")
 	
 	caderno.visible = true
-	for slot in slots:
-		if slot.texture == null:
-			slot.texture = load(carimbo_path)
-			await get_tree().create_timer(0.5).timeout
-			slot.visible = true
+	if carimbo["slot"] == "nenhum":
+		for slot in slots:
+			if slot.texture == null:
+				slot.texture = load(carimbo_path)
+				await get_tree().create_timer(0.5).timeout
+				slot.visible = true
+				carimbo["slot"] = slot.name
+				break
+	else:
+		pass
 	await get_tree().create_timer(1).timeout
 	await click
 	caderno.visible = false
